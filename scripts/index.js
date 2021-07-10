5135a7f4837e2188a5a5de3c35b6c81164a80406
megazortti <39544599+megazortti@users.noreply.github.com>
2021-07-10 19:47:02 -0300

    httpsAddiction

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const webApi = "http://179.111.62.251:5000/"
+const webApi = "https://179.111.62.251:5000/"
 
 function noticia (categoria, titulo, autor, image, text){
     this.categoria = categoria
